0f19bca9485d78b0d4a96a42eba443553c90d892
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-11-15 19:10:57 -0500

    Config .env

--- src/main/java/com/Juanes/Literalura/LiteraluraApplication.java
+++ src/main/java/com/Juanes/Literalura/LiteraluraApplication.java
@@ -2,11 +2,23 @@ package com.Juanes.Literalura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
+
+		// Cargar variables de entorno
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./") // Directorio donde está el archivo .env
+				.load();
+
+		// Variables se cargan automáticamente en el sistema
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
